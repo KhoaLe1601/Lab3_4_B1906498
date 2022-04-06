6a655e1b08d8efda0822c88afa04e4072eee5984
Khoa Le <khoab1906498@example.com>
2022-04-07 00:14:59 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -11,7 +11,7 @@
             <ContactList 
                 v-if="filteredContactsCount > 0"
                 :contacts="filteredContacts"
-                v-model:activeIndex="activeIndex"
+                v-model="activeIndex"
 
             />
             <p v-else>Không có liên hệ nào.</p>
@@ -27,6 +27,25 @@
                 <button class="btn btn-sm btn-danger" @click="removeAllContacts">
                     <i class="fas fa-trash"></i> Xóa tất cả
                 </button>
+            </div>
+        </div>
+        <div class="mt-3 col-md-6">
+            <div v-if="activeContact">
+                <h4>
+                    Chi tiết Liên hệ
+                    <i class="fas fa-address-card"></i>
+                </h4>
+                <ContactCard :contact="activeContact" />
+                <router-link
+                    :to="{
+                        name: 'contact.edit',
+                        params: { id: activeContact.id },
+                    }"
+                >
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i> Hiệu chỉnh</span
+                    >
+                </router-link>
             </div>
         </div>
     </div>
